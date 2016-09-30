ae31534d54c8f6169a6f0e2ecee33ccb69353fa1
Gux12 <gx199492@qq.com>
2016-09-30 19:13:52 +0800

    #建立github pages项目主页

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ gulp.task('webpack',function(){
         .pipe(named())
         .pipe(gulpWebpack(webpackConfig,webpack))
         .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(''))
         .pipe(browserSync.reload({stream:true}));
 });
 
