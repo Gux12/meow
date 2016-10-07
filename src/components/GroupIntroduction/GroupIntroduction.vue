52c688c4b4a030430323f78f831ad3a63e734eb4
Gux12 <gx199492@qq.com>
2016-10-08 00:11:32 +0800

    #修改了页面图片在不同设备下显示的bug

--- src/components/GroupIntroduction/GroupIntroduction.vue
+++ src/components/GroupIntroduction/GroupIntroduction.vue
@@ -16,28 +16,28 @@
                     <li data-target="#gux-carousel-example-captions" data-slide-to="3" class="active"></li>
                 </ol>
                 <div class="carousel-inner" role="listbox">
-                    <div class="item active" style="height:640px;overflow:hidden;">
+                    <div class="item active" style="overflow:hidden;">
                         <img src="./assets/img/WechatIMG4.jpeg" data-holder-rendered="true">
                         <div class="carousel-caption">
                             <h3>9月28日 星期三</h3>
                             <p>今天对需求分析进行了激烈的讨论，并确定了组内分工</p>
                         </div>
                     </div>
-                    <div class="item" style="height:640px;overflow:hidden;">
+                    <div class="item" style="overflow:hidden;">
                         <img src="./assets/img/WechatIMG2.jpeg" data-holder-rendered="true">
                         <div class="carousel-caption">
                             <h3>9月30日 星期五</h3>
                             <p>今天对STORY进行分解与描述</p>
                         </div>
                     </div>
-                    <div class="item" style="height:640px;overflow:hidden;">
+                    <div class="item" style="overflow:hidden;">
                         <img src="./assets/img/WechatIMG1.jpeg" data-holder-rendered="true">
                         <div class="carousel-caption">
                             <h3>9月30日 星期五</h3>
                             <p>今天确定了Product Backlog</p>
                         </div>
                     </div>
-                    <div class="item" style="height:640px;overflow:hidden;">
+                    <div class="item" style="overflow:hidden;">
                         <img src="./assets/img/WechatIMG3.jpeg" data-holder-rendered="true">
                         <div class="carousel-caption">
                             <h3>9月30日 星期五</h3>
@@ -58,55 +58,55 @@
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-4">
                 <div class="thumbnail">
-                    <div style="height:480px;overflow:hidden"><img data-src="holder.js/100%" src="./assets/collaborations/nanyixuan.jpeg" data-holder-rendered="true" style="width: 100%; display: block;">
+                    <div class="gux-image-container"><img class="img-responsive img-rounded" src="./assets/collaborations/nanyixuan.jpeg" data-holder-rendered="true">
                     </div>
                     <div class="caption">
                         <h3>南艺璇</h3>
-                        <p>自硕161班，CIMS所，师从柴跃廷老师。曾和顾老师一同为柴老师的新一代电子商务平台设计和开发项目做研究，努力学习产品经理、美工和码农的技能，之后将把重点转向前端开发，希望和大家共同学习</p>
+                        <div class="gux-content"><p>自硕161班，CIMS所，师从柴跃廷老师。曾和顾老师一同为柴老师的新一代电子商务平台设计和开发项目做研究，努力学习产品经理、美工和码农的技能，之后将把重点转向前端开发，希望和大家共同学习</p></div>
                         <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">186</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">3</span></a></p>
                     </div>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-4">
                 <div class="thumbnail">
-                    <div style="height:480px;overflow:hidden"><img data-src="holder.js/100%" src="./assets/collaborations/guxiang.jpeg" data-holder-rendered="true" style="width: 100%; display: block;">
+                    <div class="gux-image-container"><img class="img-responsive" src="./assets/collaborations/guxiang.jpeg" data-holder-rendered="true">
                     </div>
                     <div class="caption">
                         <h3>顾乡(Gavin Gu)</h3>
-                        <p>清华大学自动化系研究生，专业方向企业信息化系统集成，电商国家重点实验室;喜好唱歌、前端开发;项目中主要负责网页框架搭建，组内文档共享模块</p><br>
+                        <div class="gux-content"><p>清华大学自动化系研究生，专业方向企业信息化系统集成，电商国家重点实验室;喜好唱歌、前端开发;项目中主要负责网页框架搭建，组内文档共享模块</p></div>
                         <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">163</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">5</span></a></p>
                     </div>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-4">
                 <div class="thumbnail">
-                    <div style="height:480px;overflow:hidden"><img data-src="holder.js/100%" src="./assets/collaborations/puyudi.jpeg" data-holder-rendered="true" style="width: 100%; display: block;">
+                    <div class="gux-image-container"><img class="img-responsive" src="./assets/collaborations/puyudi.jpeg" data-holder-rendered="true">
                     </div>
                     <div class="caption">
-                        <h3>蒲宇狄(Best Riven Dagou Di)</h3>
-                        <p>清华大学自动化系研究生，专业方向企业信息化系统集成；项目中主要负责网页框架搭建，组内文档共享模块</p><br>
+                        <h3>蒲宇狄(Best Riven)</h3>
+                        <div class="gux-content"><p>清华大学自动化系研究生，专业方向企业信息化系统集成；项目中主要负责网页框架搭建，组内文档共享模块</p></div>
                         <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">143</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">2</span></a></p>
                     </div>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-4">
                 <div class="thumbnail">
-                    <div style="height:480px;overflow:hidden"><img data-src="holder.js/100%" src="./assets/collaborations/zhangxueheng.jpg" data-holder-rendered="true" style="width: 100%; display: block;">
+                    <div class="gux-image-container"><img class="img-responsive" src="./assets/collaborations/zhangxueheng.jpg" data-holder-rendered="true">
                     </div>
                     <div class="caption">
                         <h3>张学恒(Michael Zhang)</h3>
-                        <p>清华大学自动化系研究生，专业方向控制理论与控制工程；喜欢游泳、读书;项目中主要负责任务分配、整体协调、小组介绍模块</p><br>
+                        <div class="gux-content"><p>清华大学自动化系研究生，专业方向控制理论与控制工程；喜欢游泳、读书;项目中主要负责任务分配、整体协调、小组介绍模块</p></div>
                         <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">135</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">5</span></a></p>
                     </div>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-4">
                 <div class="thumbnail">
-                    <div style="height:480px;overflow:hidden"><img data-src="holder.js/100%" src="./assets/collaborations/xuzhiyong.jpeg" data-holder-rendered="true" style="width: 100%; display: block;">
+                    <div class="gux-image-container"><img class="img-responsive" src="./assets/collaborations/xuzhiyong.jpeg" data-holder-rendered="true">
                     </div>
                     <div class="caption">
                         <h3>徐支勇(Zyong Xu)</h3>
-                        <p>清华大学自动化系研究生，专业方向企业信息化系统集成，喜好不学习加运动，立志于成为不写代码的程序员</p><br>
+                        <div class="gux-content"><p>清华大学自动化系研究生，专业方向企业信息化系统集成，喜好不学习加运动，立志于成为不写代码的程序员</p></div>
                         <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">130</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">2</span></a></p>
                     </div>
                 </div>
@@ -116,6 +116,18 @@
 </template>
 <style>
     @import "GroupIntroduction.css";
+    .gux-image-container {
+        width: 100%;
+        height: 0;
+        padding-bottom: 140%;
+        overflow: hidden;
+    }
+    .gux-content {
+        width: 100%;
+        height: 0;
+        padding-bottom: 40%;
+        overflow: hidden;
+    }
 </style>
 <script>
     var $ = window.$ = window.jQuery = require('jquery');
