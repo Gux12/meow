7884460e8930d6b9a130c098553b1bab7cfbd56a
Michael <z4164362@126.com>
2016-10-07 13:40:30 +0800

    Update GroupIntroduction.vue

--- src/components/GroupIntroduction/GroupIntroduction.vue
+++ src/components/GroupIntroduction/GroupIntroduction.vue
@@ -13,25 +13,32 @@
                     <li data-target="#gux-carousel-example-captions" data-slide-to="2" class="active"></li>
                 </ol>
                 <div class="carousel-inner" role="listbox">
-                    <div class="item active" style="overflow:hidden;">
-                        <img src="./assets/img/WechatIMG1.jpeg" data-holder-rendered="true">
+                   <div class="item active" style="height:640px;overflow:hidden;">
+                        <img src="./assets/img/WechatIMG4.jpeg" data-holder-rendered="true">
                         <div class="carousel-caption">
-                            <h3>9月30日 星期五</h3>
-                            <p>今天进行了激烈的讨论</p>
+                            <h3>9月28日 星期三</h3>
+                            <p>今天对需求分析进行了激烈的讨论，并确定了组内分工</p>
                         </div>
                     </div>
-                    <div class="item" style="overflow:hidden;">
-                        <img src="./assets/img/WechatIMG1.jpeg" data-holder-rendered="true">
+                    <div class="item" style="height:640px;overflow:hidden;">
+                        <img src="./assets/img/WechatIMG2.jpeg" data-holder-rendered="true">
                         <div class="carousel-caption">
                             <h3>9月30日 星期五</h3>
-                            <p>今天进行了激烈的讨论</p>
+                            <p>今天对STORY进行分解与描述</p>
                         </div>
                     </div>
-                    <div class="item" style="overflow:hidden;">
+                    <div class="item" style="height:640px;overflow:hidden;">
                         <img src="./assets/img/WechatIMG1.jpeg" data-holder-rendered="true">
                         <div class="carousel-caption">
                             <h3>9月30日 星期五</h3>
-                            <p>今天进行了激烈的讨论</p>
+                            <p>今天确定了Product Backlog</p>
+                        </div>
+                    </div>
+                    <div class="item" style="height:640px;overflow:hidden;">
+                        <img src="./assets/img/WechatIMG3.jpeg" data-holder-rendered="true">
+                        <div class="carousel-caption">
+                            <h3>9月30日 星期五</h3>
+                            <p>今天确定了Product Backlog</p>
                         </div>
                     </div>
                 </div>
@@ -52,8 +59,8 @@
                     </div>
                     <div class="caption">
                         <h3>顾乡(Gavin Gu)</h3>
-                        <p>清华大学自动化系研究生，专业方向企业信息化系统集成，电商国家重点实验室，喜好唱歌、前端开发，目标大号有个妖刀姬</p>
-                        <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">4</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">0</span></a></p>
+                        <p>清华大学自动化系研究生，专业方向企业信息化系统集成，电商国家重点实验室;喜好唱歌、前端开发;项目中主要负责网页框架搭建，组内文档共享模块</p><br>
+                        <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">163</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">5</span></a></p>
                     </div>
                 </div>
             </div>
@@ -63,8 +70,19 @@
                     </div>
                     <div class="caption">
                         <h3>蒲宇狄(Best Riven Dagou Di)</h3>
-                        <p>清华大学自动化系研究生，专业方向企业信息化系统集成，清华第一瑞雯，世界第一阴阳师肝帝</p><br>
-                        <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">4</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">0</span></a></p>
+                        <p>清华大学自动化系研究生，专业方向企业信息化系统集成；项目中主要负责网页框架搭建，组内文档共享模块</p><br>
+                        <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">143</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">2</span></a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <div style="height:480px;overflow:hidden"><img data-src="holder.js/100%" src="./assets/collaborations/zhangxueheng.jpg" data-holder-rendered="true" style="width: 100%; display: block;">
+                    </div>
+                    <div class="caption">
+                        <h3>张学恒(Michael Zhang)</h3>
+                        <p>清华大学自动化系研究生，专业方向控制理论与控制工程；喜欢游泳、读书;项目中主要负责任务分配、整体协调、小组介绍模块</p><br>
+                        <p><a href="#" class="btn btn-danger" role="button">喜欢 <span class="badge">127</span></a> <a href="#" class="btn btn-default" role="button">不喜欢 <span class="badge">5</span></a></p>
                     </div>
                 </div>
             </div>
