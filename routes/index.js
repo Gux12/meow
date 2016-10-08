635d074e793036e5749c2573d2fa5a22c00e4836
Gux12 <gx199492@qq.com>
2016-10-08 20:19:08 +0800

    #添加了服务器以及文件上传功能

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var multiparty = require('multiparty');
+var util = require('util');
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html');
+});
+
+/* 上传*/
+router.post('/file/uploading', function(req, res, next){
+  //生成multiparty对象，并配置上传目标路径
+  var form = new multiparty.Form({uploadDir: './public/files/'});
+  //上传完成后处理
+  form.parse(req, function(err, fields, files) {
+    var filesTmp = JSON.stringify(files,null,2);
+
+    if(err){
+      console.log('parse error: ' + err);
+    } else {
+      console.log('parse files: ' + filesTmp);
+      var inputFile = files.upfile[0];
+      var uploadedPath = inputFile.path;
+      var dstPath = './public/files/' + inputFile.originalFilename;
+      //重命名为真实文件名
+      fs.rename(uploadedPath, dstPath, function(err) {
+        if(err){
+          console.log('rename error: ' + err);
+        } else {
+          console.log('rename ok');
+          fs.readdir('./public/files/', (err, files) => {
+            if (err) throw err;
+            console.log('json ok');
+            res.json(files);
+          });
+        }
+      });
+    }
+
+    // res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
+    // res.write('received upload:\n\n');
+    // res.end(util.inspect({fields: fields, files: filesTmp}));
+  });
+});
+
+router.get('/getlist', function(req, res, next){
+  fs.readdir('./public/files/', (err, files) => {
+    if (err) throw err;
+    res.json(files);
+  });
+})
+
+module.exports = router;
--- src/components/GroupDocumentExchange/GroupDocumentExchange.vue
+++ src/components/GroupDocumentExchange/GroupDocumentExchange.vue
@@ -5,16 +5,55 @@
             <h4>小组的文档交流可以在下面进行</h4>
             <p>小组成员可以上传自己需要共享的文件，其他小组成员可以在其上下载查看</p>
         </div>
-        <input id="fileupload" type="file" name="files[]" data-url="server/php/" multiple>
-        <div id="progress">
-            <div class="bar" style="width: 0%;"></div>
-        </div>
+        <form id="gux-fileupload-form" class="form-inline">
+            <div class="form-group">
+                <a class="btn btn-success" id="gux-file-a">选择文件<input type="file" name="upfile" multiple id="gux-fileupload-file"/></a>
+                <button class="btn btn-default" id="gux-fileupload-submit-btn">上传</button>
+            </div>
+            <div class="form-group">
+                <label class="sr-only" for="gux-filename">filename</label>
+                <input class="form-control" id="gux-filename" disabled placeholder="请选择文件">
+            </div>
+        </form>
+        <table class="table" id="gux-file-table">
+            <caption>文件列表</caption>
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>文件名</th>
+                <th>操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            <!--<tr>-->
+                <!--<td>1</td>-->
+                <!--<td>Mark</td>-->
+                <!--<td>Otto</td>-->
+            <!--</tr>-->
+            </tbody>
+        </table>
     </div>
 </template>
 <style>
     .bar {
         height: 18px;
         background: green;
+    }
+    #gux-file-a{
+        display:inline-block;
+        position:relative;
+        overflow:hidden;
+    }
+    /*#gux-file-a:hover{*/
+        /*background:green;*/
+    /*}*/
+    #gux-fileupload-file{
+        position:absolute;
+        right:0;
+        top:0;
+        font-size:100px;
+        opacity:0;
+        filter:alpha(opacity=0);
     }
 </style>
 <script>
@@ -26,23 +65,53 @@
             }
         },
         mounted:function() {
-            $(function () {
-                $('#fileupload').fileupload({
-                    dataType: 'json',
-                    done: function (e, data) {
-                        $.each(data.result.files, function (index, file) {
-                            $('<p/>').text(file.name).appendTo(document.body);
-                        });
-                    },
-                    progressall: function (e, data) {
-                        var progress = parseInt(data.loaded / data.total * 100, 10);
-                        $('#progress .bar').css(
-                                'width',
-                                progress + '%'
-                        );
+            $.ajax({
+                url:'/getlist',
+                type: 'get',
+                dataType: 'json',
+                success:function(data){
+                    var i=1;
+                    for (var filename of data) {
+                        $('#gux-file-table').find('tbody').append('<tr><td>'+i+'</td><td>'+filename+'</td><td><a class="btn btn-info" href="/files/'+filename+'">下载</a></td></tr>');
+                        i++;
                     }
-                });
+                },
+                error:function(data){
+                    alert('无法访问服务器文件列表！');
+                }
+            })
+
+            $("#gux-fileupload-submit-btn").click(()=>{
+                var filename = $(this).find("#gux-fileupload-file").val();
+                if(filename != 'undefine' && filename == ''){
+                    var formData = new FormData($("#gux-fileupload-form")[0]);
+                    $.ajax({
+                        url : "/file/uploading",
+                        type : "post",
+                        data : formData,
+                        async: false,
+                        cache: false,
+                        contentType: false,
+                        processData: false,
+                        success : function(data) {
+                            $('#gux-file-table').find('tbody')[0].innerHTML = '';
+                            var i=1;
+                            for (var filename of data) {
+                                $('#gux-file-table').find('tbody').append('<tr><td>'+i+'</td><td>'+filename+'</td><td><a class="btn btn-info" href="/files/'+filename+'">下载</a></td></tr>');
+                                i++;
+                            }
+                        },
+                    });
+                }else{
+                    console.log($(this).find("#gux-fileupload-file").val());
+                    alert('上传文件不能为空！');
+                }
+
             });
+
+            $('#gux-fileupload-file').change(function(){
+                $('#gux-filename')[0].value = $(this).val();
+            })
         }
     }
 </script>
